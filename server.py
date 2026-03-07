c17669810992549f5068396d851b3819da90340d
manish1525 <manishkumarkhairnar53@gmail.com>
2026-03-07 10:47:29 +0530

    Update server.py

--- server.py
+++ server.py
@@ -311,4 +311,7 @@ if __name__ == '__main__':
     print("Admin: http://127.0.0.1:5000/admin")
     print("=" * 55)
 
-    app.run(debug=True, port=5000, host='0.0.0.0')
+    import os
+    port = int(os.environ.get("PORT", 5000))
+
+    app.run(debug=True, host='0.0.0.0', port=port)
